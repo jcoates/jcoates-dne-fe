53396002fd0e26b9df2597a6ff136fefce30752d
jcoates <me@jcoat.es>
2020-01-20 13:52:21 -0500

    Update front end to function with localhost.
    This set of changes replaces the function for ImgMediaCard with a
    class, which caused me to need to redefine some things.
    
    This change was made so I could hook into onComponentDidMount, so the
    image that starts on the page could be dynamically loaded in the right
    place in the lifecycle.
    
    This also changes the functionality such that the app fetches an image
    from my localhost api. Imagining you have a localhost api that can
    produce the images you want to load, this all functions now.

--- src/ImageMediaCard.js
+++ src/ImageMediaCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import IconButton from '@material-ui/core/IconButton';
 
-const useStyles = makeStyles(theme => ({
+const styles = ({
   card: {
       marginLeft: 'auto',
       marginRight: 'auto',
@@ -20,41 +21,57 @@ const useStyles = makeStyles(theme => ({
   },
   controls: {
     alignItems: 'center',
-    padding: theme.spacing(1),
+    padding: 12,
   },
   refreshIcon: {
     height: '5vh',
     width: '5vh',
   },
-}));
+});
 
-export default function ImgMediaCard() {
-  const classes = useStyles();
-  const [image, setImage] = React.useState('000023.png');
+class ImgMediaCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: ''
+    };
+  }
 
-  const handleRefresh = () => {
-    if (image === '000023.png') {
-      setImage('002300.png'); 
-    } else {
-      setImage('000023.png'); 
-    }
-    
+  async handleRefresh() {
+    const jsonResponse = await fetch('http://127.0.0.1:5000/dne').then((response) => {
+      return response.json();
+    });
+
+    this.setState({image: jsonResponse['image_url']});
   };
 
+  componentDidMount() {
+    this.handleRefresh();
+  }
 
-  return (
-    <Card className={`sized-card ${classes.card}`}>
-        <CardMedia
-          component="img"
-          alt="JCoates"
-          image={image}
-          title="JCoates"
-        />
-      <div className={classes.controls}>
-          <IconButton aria-label="Regenerate">
-            <RefreshIcon className={classes.refreshIcon} onClick={handleRefresh}/>
-          </IconButton>
-        </div>
-    </Card>
-  );
+  render() {
+    const { classes } = this.props;
+    return (
+      <Card className={`sized-card ${classes.card}`}>
+          <CardMedia
+            component="img"
+            alt="JCoates"
+            image={this.state.image}
+            title="JCoates"
+          />
+        <div className={classes.controls}>
+            <IconButton aria-label="Regenerate" onClick={this.handleRefresh.bind(this)}>
+              <RefreshIcon className={classes.refreshIcon}/>
+            </IconButton>
+          </div>
+      </Card>
+    );
+  }
 }
+
+ImgMediaCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(ImgMediaCard);
